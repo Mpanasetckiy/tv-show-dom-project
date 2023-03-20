c093680138ead679efdf0011d38bc62c5c6b8312
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-03-20 01:25:11 +0000

    level 100 done

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-//You can edit ALL of the code here
+//Get DOM elements
+const rootElem = document.getElementById("root");
+const eachEpisode = document.querySelector('#episode');
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  console.log(episodeList)
+  let result = '';
+  episodeList.forEach(episode => {
+    let episodeNum = episode.number < 10? `0${episode.number}` : episode.number;
+    result += `
+    <div class="episode__wrap">
+        <div class="episode__header">
+          <h3>${episode.name} - S0${episode.season}E${episodeNum}</h3>
+        </div>
+        <div class="episode__info">
+          <img src="${episode.image.medium}" alt="">
+          ${episode.summary}
+        </div>
+    </div>`
+  })
+  episode.innerHTML = result
 }
 
 window.onload = setup;
+
